1d2a89777abf95e39438190a11105e5025a52939
Kenny Chung <chunghlken@gmail.com>
2018-03-15 21:53:47 -1000

    #8 solution

--- exercises.js
+++ exercises.js
@@ -249,6 +249,20 @@ printOrders(arrayOfObjects);
         to inspect your results.
 */
 
+var sumObj = {
+  a: 2,
+  b: 3,
+  result: undefined,
+};
+
+function objectAddition(obj){
+  obj.result = obj.a + obj.b;
+  return obj;
+}
+
+var sumObjResult = objectAddition(sumObj);
+console.log(sumObjResult);
+
 
 /*
 9. Print sum function and add as new key-value
