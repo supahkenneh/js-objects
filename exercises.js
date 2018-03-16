dbe2458c752e065c1ad86ceaac143facbeb3474d
Kenny Chung <chunghlken@gmail.com>
2018-03-15 21:23:35 -1000

    #6 solution

--- exercises.js
+++ exercises.js
@@ -132,6 +132,18 @@ console.log(stockCar);
    the value at `name`, and just the value at `age`.
  */
 
+var plainPerson = {};
+
+function buildPerson(person, nameString, age){
+  plainPerson.name = nameString;
+  plainPerson.age = age;
+  return person;    
+}
+
+var completePerson = buildPerson(plainPerson, 'Kenny', 25);
+console.log(completePerson);
+console.log(completePerson.name);
+console.log(completePerson.age);
 
 /*
 7. Display values of objects that are inside an array
